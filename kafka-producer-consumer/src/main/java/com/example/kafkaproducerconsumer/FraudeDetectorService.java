f91f3a8169ae0f0e0f77920b20b4d56194fbef94
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 20:49:32 -0300

    Adicionado no properties o Group-Id pra receber as mensagens do new order

--- kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/FraudeDetectorService.java
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/FraudeDetectorService.java
@@ -33,6 +33,7 @@ public class FraudeDetectorService {
             } catch (InterruptedException e){
                 e.printStackTrace();
             }
+            System.out.println("Order processed");
 
         }
     }
@@ -43,6 +44,7 @@ public class FraudeDetectorService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudeDetectorService.class.getName());
         return properties;
     }
 }
