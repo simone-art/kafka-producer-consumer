2a7291e6bb75db87c5d23e006bca7e83764f5428
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 12:17:41 -0300

    Adicionando a propiedade Key serializer que transforma string em bytes

--- kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
@@ -2,6 +2,7 @@ package com.example.kafkaproducerconsumer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
 import java.util.Properties;
@@ -17,6 +18,7 @@ public class NewOrder {
     private static Properties properties() {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
     }
 }
