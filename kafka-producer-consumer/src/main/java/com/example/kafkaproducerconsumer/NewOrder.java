c9467e690649b6d0595c3c8136c900d2db8c0ac3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 12:19:47 -0300

    Adicionada a propiedad Value serializer class config com a funcao de tranformar strings em bytes

--- kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
@@ -19,6 +19,7 @@ public class NewOrder {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
     }
 }
