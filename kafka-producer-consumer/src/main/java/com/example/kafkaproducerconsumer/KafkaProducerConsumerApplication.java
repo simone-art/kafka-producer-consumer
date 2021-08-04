ee1a4ef87233101bb3bad3ec5eb7bce6ffa04a6a
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 11:36:55 -0300

    First commit

---
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/KafkaProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerConsumerApplication.class, args);
+	}
+
+}
