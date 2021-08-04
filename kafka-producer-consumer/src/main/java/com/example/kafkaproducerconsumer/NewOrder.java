72adc1af6302974493b950721abd0ca1beafb757
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 12:31:46 -0300

    Adicionado código pra definir a chave e o valor na variável value

--- kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/NewOrder.java
@@ -2,6 +2,7 @@ package com.example.kafkaproducerconsumer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
@@ -10,8 +11,14 @@ import java.util.Properties;
 public class NewOrder {
 
     //Kafka precisa de propiedades para definir onde será a conexao
+    //Mensagem que o producer vai enviar tem que ser gravada; Por isso se usa o record
+    //Producer record usa também os parámetros de chave e valor
+    // Na variável record vai ser definido o topic
     public static void main(String[] args) {
         var producer = new KafkaProducer<String, String>(properties());
+        var value = "12345, 76523, 300";
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value );
+        producer.send(record);
     }
 
     //criado método estático
