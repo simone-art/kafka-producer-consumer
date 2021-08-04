9ddf370cd84bfa03827f74c5106db906e17e56f0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-08-04 19:41:24 -0300

    Alterado código

--- kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/KafkaProducerConsumerApplication.java
+++ kafka-producer-consumer/src/main/java/com/example/kafkaproducerconsumer/KafkaProducerConsumerApplication.java
@@ -3,11 +3,11 @@ package com.example.kafkaproducerconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class KafkaProducerConsumerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaProducerConsumerApplication.class, args);
-	}
-
-}
+//@SpringBootApplication
+//public class KafkaProducerConsumerApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(KafkaProducerConsumerApplication.class, args);
+//	}
+//
+//}
